f7792636ad800edae9e2295a7e2d32d311e0c586
DMG <dmwaigithinji@gmail.com>
2021-03-27 10:53:04 +0300

    Inititalized nx monorepo with angular app

--- apps/ng-scrappy/src/app/app.component.ts
+++ apps/ng-scrappy/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'scappy.org-root',
+  selector: 'ng-scrappy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
