11b603aa0c1297bdd2f40c8f485ea71bfaed698d
DMG <dmwaigithinji@gmail.com>
2021-03-27 10:46:08 +0300

    initial commit

---
+++ apps/ng-scrappy/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scappy.org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-scrappy';
+}
