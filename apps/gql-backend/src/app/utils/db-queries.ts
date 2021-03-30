269ab071177160f5961fd7b4eefbd4f2c8a4b7fb
DMG <dmwaigithinji@gmail.com>
2021-03-30 22:46:45 +0300

    Enable sort by word in gql

--- apps/gql-backend/src/app/utils/db-queries.ts
+++ apps/gql-backend/src/app/utils/db-queries.ts
@@ -1,55 +1,60 @@
 import * as _ from 'lodash';
 
-
 /**
  * Queries a word's translations from the different lang collections
  * @param {FirebaseFirestore} db
  * @return {string[]} List of languages
  */
- export async function searchWord(db: FirebaseFirestore.Firestore, word: string) {
+export async function searchWord(
+  db: FirebaseFirestore.Firestore,
+  word: string
+) {
   const langObjs = await getSupportedLangs(db);
-  const langs = langObjs.map(obj => obj.language);
+  const langs = langObjs.map((obj) => obj.language);
 
   const res = await Promise.all(
     langs.map(async (lang) => {
       const snapshot = await db
         .collection(`dictionary/${lang}/words`)
         .where('word', '==', word)
+        .orderBy('word')
         .get();
-      return snapshot.docs.map((doc) => {
-        const trl = doc.data();
-        trl.language = lang;
-        return trl;
+      const trl = snapshot.docs.map((doc) => {
+        const t = doc.data();
+        t.language = lang;
+        return t;
       });
-    }))
+      return trl;
+    })
+  );
 
-    return _.flatMap(res);
+  return _.flatMap(res);
 }
-
 
 /**
  * Queries words in a language collection
  * @param {FirebaseFirestore} db
  * @return {string[]} List of Translation Objects
  */
- export async function getLangWords(db: FirebaseFirestore.Firestore, lang: string) {
-  const trls = await queryCollection(db, `dictionary/${lang}/words`);
-  return trls.map(trl => {
+export async function getLangWords(
+  db: FirebaseFirestore.Firestore,
+  lang: string
+) {
+  const trls = await queryCollection(db, `dictionary/${lang}/words`, 'word');
+  return trls.map((trl) => {
     trl.language = lang;
     return trl;
-  })
+  });
 }
-
 
 /**
  * Queries supported languages
  * @param {FirebaseFirestore} db
  * @return {string[]} List of languages
  */
- export async function getSupportedLangs(db: FirebaseFirestore.Firestore) {
+export async function getSupportedLangs(db: FirebaseFirestore.Firestore) {
   return await queryCollection(db, 'supported-languages');
 }
-
 
 /**
  * Queries data for the given collection path
@@ -57,7 +62,12 @@ import * as _ from 'lodash';
  * @param {string} collectionPath
  * @return {DocumentData} document data
  */
- export async function queryCollection(db: FirebaseFirestore.Firestore, collectionPath: string) {
-  const snapshot = await db.collection(collectionPath).get();
+export async function queryCollection(
+  db: FirebaseFirestore.Firestore,
+  collectionPath: string,
+  orderProp?: string
+) {
+  const orderBy = orderProp ?? 'createdAt';
+  const snapshot = await db.collection(collectionPath).orderBy(orderBy).get();
   return snapshot.docs.map((doc) => doc.data());
 }
