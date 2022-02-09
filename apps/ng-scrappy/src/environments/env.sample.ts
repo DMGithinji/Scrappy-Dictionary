1559ebd50051960191ae2cc929bd0b83ab756d9a
DMGithinji <dmwaigithinji@gmail.com>
2022-02-09 12:44:03 +0300

    include sample environment files

---
+++ apps/ng-scrappy/src/environments/env.sample.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  production: false, // true or false based on whether you want to run the app in production mode
+  algoliaConfig: {
+    appId: "",
+    searchKey: "",
+  },
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId:""
+  },
+
+};
